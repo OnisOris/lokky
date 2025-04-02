c8aa030020bb5e6717dc9c41e53cff063b14e583
Michael <onisoris@yandex.ru>
2025-04-02 21:51:53 +0300

    Fix errors in algorithms and update fields in class

--- src/lokky/pionmath.py
+++ src/lokky/pionmath.py
@@ -51,9 +51,9 @@ def limit_acceleration(
     return current_velocity + change
 
 
-def find_points_in_radius(center_sphere: NDArray,
-                          points: NDArray,
-                          radius: float = 1.) -> NDArray:
+def find_points_in_radius(
+    center_sphere: NDArray, points: NDArray, radius: float = 1.0
+) -> NDArray:
     """
     Find points within a sphere.
 
@@ -70,9 +70,9 @@ def find_points_in_radius(center_sphere: NDArray,
     :rtype: NDArray
     """
     tx, ty, tz = center_sphere
-    radius_sq = radius ** 2
+    radius_sq = radius**2
     result = np.zeros(3)
-    for (x, y, z) in points:
+    for x, y, z in points:
         dx = x - tx
         dy = y - ty
         dz = z - tz
@@ -105,18 +105,20 @@ def rot_v(vector: np.ndarray, angle: float, axis: np.ndarray) -> np.ndarray:
     c = cos(angle)
     s = sin(angle)
     t = 1 - c
-    rotate = np.array([
-        [t * x ** 2 + c,    t * x * y - s * z, t * x * z + s * y],
-        [t * x * y + s * z, t * y ** 2 + c,    t * y * z - s * x],
-        [t * x * z - s * y, t * y * z + s * x, t * z ** 2 + c]
-    ])
+    rotate = np.array(
+        [
+            [t * x**2 + c, t * x * y - s * z, t * x * z + s * y],
+            [t * x * y + s * z, t * y**2 + c, t * y * z - s * x],
+            [t * x * z - s * y, t * y * z + s * x, t * z**2 + c],
+        ]
+    )
     rot_vector = np.dot(vector, rotate)
     return rot_vector
 
 
-def check_point_in_radius(center_sphere: NDArray,
-                          point: NDArray,
-                          radius: float = 1.) -> tuple[bool, float, NDArray]:
+def check_point_in_radius(
+    center_sphere: NDArray, point: NDArray, radius: float = 1.0
+) -> tuple[bool, float, NDArray]:
     """
     Check if a point is within a sphere and return distance and vector.
 
@@ -166,6 +168,7 @@ class SSolver:
     and A is a matrix in R^(n x 6).
     When running on a drone, x[0] corresponds to the drone's state vector.
     """
+
     def __init__(self, params: dict):
         """
         Initialize the parameters.
@@ -173,6 +176,19 @@ class SSolver:
         :param params: Dictionary of parameters.
         :type params: dict
         """
+        self.kd = None
+        self.ki = None
+        self.kp = None
+        self.max_acceleration = None
+        self.max_speed = None
+        self.safety_radius = None
+        self.noise_weight = None
+        self.unstable_weight = None
+        self.repulsion_weight = None
+        self.alignment_weight = None
+        self.attraction_weight = None
+        self.cohesion_weight = None
+        self.unstable_radius = None
         self.params = params
         if self.params is None:
             self.params = {
@@ -202,23 +218,23 @@ class SSolver:
         :type params: dict
         """
         self.params = params
-        self.attraction_weight: float = params['attraction_weight']
-        self.cohesion_weight: float = params['cohesion_weight']
-        self.alignment_weight: float = params['alignment_weight']
-        self.repulsion_weight: float = params['repulsion_weight']
-        self.unstable_weight: float = params['unstable_weight']
-        self.noise_weight: float = params['noise_weight']
-        self.safety_radius: float = params['safety_radius']
-        self.max_speed: float = params['max_speed']
-        self.max_acceleration: float = params['max_acceleration']
-        self.kp = np.array(params['kp'])
-        self.ki = np.array(params['kp'])
-        self.kd = np.array(params['kp'])
+        self.attraction_weight: float = params["attraction_weight"]
+        self.cohesion_weight: float = params["cohesion_weight"]
+        self.alignment_weight: float = params["alignment_weight"]
+        self.repulsion_weight: float = params["repulsion_weight"]
+        self.unstable_weight: float = params["unstable_weight"]
+        self.noise_weight: float = params["noise_weight"]
+        self.safety_radius: float = params["safety_radius"]
+        self.max_speed: float = params["max_speed"]
+        self.max_acceleration: float = params["max_acceleration"]
+        self.unstable_radius: float = params["unstable_radius"]
+        self.kp = np.array(params["kp"])
+        self.ki = np.array(params["ki"])
+        self.kd = np.array(params["kd"])
 
-    def solve(self,
-              state_matrix: NDArray,
-              target_matrix: NDArray,
-              dt: float) -> NDArray:
+    def solve(
+        self, state_matrix: NDArray, target_matrix: NDArray, dt: float
+    ) -> NDArray:
         """
         Solve for the control velocity for each object based on the state matrix.
 
@@ -251,13 +267,15 @@ class SSolver:
         self.previous_error = error
         vda = self.compute_velocity_direction_all(state_matrix, error)
         # Final control signal
-        control_signal = p_term[:, 0:3] + i_term[:, 0:3] + d_term[:, 0:3] + vda 
+        control_signal = saturation(
+            p_term[:, 0:3] + i_term[:, 0:3] + d_term[:, 0:3], 1
+        )
+        control_signal += vda
         return control_signal
 
-    def compute_velocity_direction_all(self, 
-                                         state_matrix: NDArray,
-                                         error_matrix: NDArray
-                                         ) -> NDArray:
+    def compute_velocity_direction_all(
+        self, state_matrix: NDArray, error_matrix: NDArray
+    ) -> NDArray:
         """
         Compute the control velocity for all state vectors.
 
@@ -272,17 +290,22 @@ class SSolver:
         """
         control_velocity_matrix = np.zeros(3)
         for index, _ in enumerate(state_matrix):
-            control_velocity_matrix = np.vstack([
-                control_velocity_matrix,
-                self.compute_velocity_direction(index, state_matrix, error_matrix)
-            ])
+            control_velocity_matrix = np.vstack(
+                [
+                    control_velocity_matrix,
+                    self.compute_velocity_direction(
+                        index, state_matrix, error_matrix
+                    ),
+                ]
+            )
         return control_velocity_matrix[1:]
-            
-    def compute_velocity_direction(self, 
-                                   index_current_state_vector: int,
-                                   state_matrix: NDArray,
-                                   error_matrix: NDArray
-                                   ) -> NDArray:
+
+    def compute_velocity_direction(
+        self,
+        index_current_state_vector: int,
+        state_matrix: NDArray,
+        error_matrix: NDArray,
+    ) -> NDArray:
         """
         Compute the control velocity for a single state vector.
 
@@ -300,53 +323,64 @@ class SSolver:
         """
         repulsion_force = np.zeros(3)
         unstable_vector = np.zeros(3)
-        state_vector = np.zeros(3)
-        for index, state_vector in enumerate(state_matrix):
-            # Skip self
-            if index == index_current_state_vector:
+        current_state = state_matrix[index_current_state_vector]
+        current_error_norm = np.linalg.norm(
+            error_matrix[index_current_state_vector][0:3]
+        )
+        state_matrix = self.state_filter(
+            current_state, state_matrix, self.safety_radius * 3
+        )
+        for j, other_state in enumerate(state_matrix):
+            if j == index_current_state_vector:
                 continue
-            repulsion_force = np.zeros(3)
-            unstable_vector = np.zeros(3)
-            check_in_sphere, dist_to_object, vector_to_object = check_point_in_radius(
-                state_matrix[index_current_state_vector][0:3],
-                state_matrix[index][0:3],
-                self.safety_radius
+            check_in_sphere, dist_to_object, vector_to_object = (
+                check_point_in_radius(
+                    current_state[0:3], other_state[0:3], self.safety_radius
+                )
             )
             vector_to_object_norm = normalization(vector_to_object, 1)
             if check_in_sphere:
-                repulsion_force += vector_to_object_norm / (
+                repulsion_force -= vector_to_object_norm / (
                     (dist_to_object + 1 - self.safety_radius) ** 2
                 )
             if self.term_count_unstable_vector(
-                dist_to_object,
-                np.linalg.norm(error_matrix[index][0:3]),
-                state_vector[3:6]
+                dist_to_object, current_error_norm, current_state[3:6]
             ):
                 unstable_vector += rot_v(
                     vector_to_object_norm * 0.3,
                     -np.pi / 2,
-                    axis=np.array([0, 0, 1])
+                    axis=np.array([0, 0, 1]),
                 )
-        # Compute the new velocity vector
         new_velocity = (
-            state_vector[3:6]
+            current_state[3:6]
             + self.repulsion_weight * repulsion_force
             + self.unstable_weight * unstable_vector
         )
-        # Limit the change (acceleration) to max_acceleration
         new_velocity = limit_acceleration(
-            state_matrix[index][3:6],
+            current_state[3:6],
             new_velocity,
             max_acceleration=self.max_acceleration,
         )
-        # Limit the speed to max_speed
         new_velocity = saturation(new_velocity, self.max_speed)
+
         return new_velocity
 
-    def term_count_unstable_vector(self,
-                                   dist_to_other_drone: float,
-                                   error: float,
-                                   speed: NDArray) -> bool:
+    def state_filter(
+        self, current_state, state_matrix: NDArray, filter_radius: float
+    ) -> NDArray:
+        state_matrix_filtered = np.zeros(state_matrix.shape[1])
+        for other_state in state_matrix:
+            if check_point_in_radius(
+                current_state[0:3], other_state[0:3], filter_radius
+            ):
+                state_matrix_filtered = np.vstack(
+                    [state_matrix_filtered, other_state]
+                )
+        return state_matrix_filtered[1:]
+
+    def term_count_unstable_vector(
+        self, dist_to_other_drone: float, error: float, speed: NDArray
+    ) -> bool:
         """
         Determine whether to add an unstable vector component based on distance, error, and speed.
 
